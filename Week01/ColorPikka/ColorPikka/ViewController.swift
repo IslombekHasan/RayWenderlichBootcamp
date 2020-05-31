20b03af9d21cc46ddea8f887c00d88a9c0538023
Islombek Khasanov <islombekhasanov@gmail.com>
2020-06-01 00:28:29 +0500

    feat: changes view's background color as the sliders are moved

--- Week01/ColorPikka/ColorPikka/ViewController.swift
+++ Week01/ColorPikka/ColorPikka/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var redStackView: UIStackView!
     @IBOutlet weak var greenStackView: UIStackView!
     @IBOutlet weak var blueStackView: UIStackView!
-
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews(for: UIScreen.main.bounds.size)
@@ -23,7 +28,27 @@ class ViewController: UIViewController {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         updateViews(for: size)
-
+    }
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        print("RED:  \(sender.value.rounded())")
+        changeBackgroundColor()
+    }
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        print("green:  \(sender.value.rounded())")
+        changeBackgroundColor()
+    }
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        print("blue:  \(sender.value.rounded())")
+        changeBackgroundColor()
+    }
+    
+    func changeBackgroundColor() {
+        let red = redSlider.value.rounded() / 255
+        let green = greenSlider.value.rounded() / 255
+        let blue = blueSlider.value.rounded() / 255
+        
+        self.view.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
     }
 
     func updateViews(for size: CGSize) {
@@ -34,7 +59,7 @@ class ViewController: UIViewController {
             updateViewsForPortrait()
         }
     }
-
+    
     func updateViewsForLandscape() {
         headerStackView.axis = .horizontal
         redStackView.spacing = 2
