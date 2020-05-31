cef791cf03f3ac18a8fbe0a22761fa0bd9ec7f55
Islombek Khasanov <islombekhasanov@gmail.com>
2020-05-31 17:36:39 +0500

    feat: add a LaunchScreen and Icons

---
+++ Week01/ColorPikka/ColorPikka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPikka
+//
+//  Created by Islombek Hasanov on 5/31/20.
+//  Copyright © 2020 Islombek Khasanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
