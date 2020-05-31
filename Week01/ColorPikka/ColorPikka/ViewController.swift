2103aa2e41383bf11ea5c12139e258ae536d1431
Islombek Khasanov <islombekhasanov@gmail.com>
2020-06-01 00:01:15 +0500

    feat: adds views, their constraints and their behavior for different orientations

--- Week01/ColorPikka/ColorPikka/ViewController.swift
+++ Week01/ColorPikka/ColorPikka/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerStackView: UIStackView!
+    @IBOutlet weak var redStackView: UIStackView!
+    @IBOutlet weak var greenStackView: UIStackView!
+    @IBOutlet weak var blueStackView: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateViews(for: UIScreen.main.bounds.size)
     }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateViews(for: size)
+
+    }
+
+    func updateViews(for size: CGSize) {
+        let isLandscape = size.width > size.height
+        if isLandscape {
+            updateViewsForLandscape()
+        } else {
+            updateViewsForPortrait()
+        }
+    }
+
+    func updateViewsForLandscape() {
+        headerStackView.axis = .horizontal
+        redStackView.spacing = 2
+        greenStackView.spacing = 2
+        blueStackView.spacing = 2
+
+    }
+
+    func updateViewsForPortrait() {
+        headerStackView.axis = .vertical
+        redStackView.spacing = 8
+        greenStackView.spacing = 8
+        blueStackView.spacing = 8
+    }
 
 }
 
