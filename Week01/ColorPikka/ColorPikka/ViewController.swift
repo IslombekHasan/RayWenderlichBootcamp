882f8d710ef2ab19560380b21a4e7fde7ce662bf
Islombek Khasanov <islombekhasanov@gmail.com>
2020-06-01 19:42:26 +0500

    feat: adds styling

--- Week01/ColorPikka/ColorPikka/ViewController.swift
+++ Week01/ColorPikka/ColorPikka/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var modeControl: UISegmentedControl!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var setColorButton: UIButton!
+    @IBOutlet weak var infoButton: UIButton!
 
     @IBOutlet weak var headerStackView: UIStackView!
     @IBOutlet weak var slidersContainer: UIView!
@@ -22,6 +25,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
 
+    var allLabels: [UILabel] = []
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
@@ -30,11 +34,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentGreenValueLabel: UILabel!
     @IBOutlet weak var currentBlueValueLabel: UILabel!
 
-    var allLabels: [UILabel] = []
+    @IBOutlet weak var headerTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var resetBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var setColorBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var infoBottomConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        slidersContainer.layer.cornerRadius = 8
+        styleViews()
         updateViews(for: UIScreen.main.bounds.size)
         setAllLabels()
     }
@@ -76,7 +83,7 @@ class ViewController: UIViewController {
         let abracadabra = UIAlertAction(title: "Abracadabra!", style: .default, handler: {
             action in
             let colorTitle = alert.textFields![0].text
-            self.headerLabel.text = colorTitle!.isEmpty ? "ColorPikka!" : colorTitle
+            self.headerLabel.text = colorTitle!.isEmpty ? "😡 PikaPika! No cute name!" : colorTitle
             self.changeBackgroundColor(forAll: true)
         })
 
@@ -92,34 +99,10 @@ class ViewController: UIViewController {
         blueSlider.setValue(0, animated: true)
 
         UIView.animate(withDuration: 0.5, animations: {
+            self.headerLabel.text = "Move sliders to see magic!"
             self.view.backgroundColor = UIColor.systemYellow
-            self.slidersContainer.backgroundColor = UIColor.clear
+            self.slidersContainer.backgroundColor = UIColor.systemYellow
         })
-    }
-
-    func updateViews(for size: CGSize) {
-        let isLandscape = size.width > size.height
-        if isLandscape {
-            updateViewsForLandscape()
-        } else {
-            updateViewsForPortrait()
-        }
-    }
-
-    func updateViewsForLandscape() {
-        headerStackView.axis = .horizontal
-        headerLabel.textAlignment = .left
-        redStackView.spacing = 2
-        greenStackView.spacing = 2
-        blueStackView.spacing = 2
-    }
-
-    func updateViewsForPortrait() {
-        headerStackView.axis = .vertical
-        headerLabel.textAlignment = .center
-        redStackView.spacing = 8
-        greenStackView.spacing = 8
-        blueStackView.spacing = 8
     }
 
     func changeBackgroundColor(forAll: Bool = false) {
@@ -139,6 +122,7 @@ class ViewController: UIViewController {
 
         changeFontColor(basedOn: color, forAll: forAll)
         slidersContainer.backgroundColor = color
+
         if forAll {
             UIView.animate(withDuration: 0.5, animations: {
                 self.view.backgroundColor = color
@@ -147,20 +131,81 @@ class ViewController: UIViewController {
     }
 
     func changeFontColor(basedOn backgroundColor: UIColor, forAll: Bool = true) {
-        // First we get luminance/brightness for the color following this algorhythm. https://stackoverflow.com/a/1855903/7403467
+        //  luminance/brightness algorhythm from https://stackoverflow.com/a/1855903/7403467
         let rgba = backgroundColor.rgba
         let luminance = (0.299 * rgba.red + 0.587 * rgba.green + 0.114 * rgba.blue)
 
-        // then set font colors based on that value
         for label in allLabels {
             if !forAll && label == headerLabel { continue }
-
             if luminance > 0.5 { // bright colors
                 label.textColor = .black
             } else {
                 label.textColor = .white
             }
         }
+    }
+
+    func updateViews(for size: CGSize) {
+        let isLandscape = size.width > size.height
+        if isLandscape {
+            updateViewsForLandscape()
+        } else {
+            updateViewsForPortrait()
+        }
+    }
+
+    func updateViewsForLandscape() {
+        headerStackView.axis = .horizontal
+        headerLabel.textAlignment = .left
+        redStackView.spacing = 2
+        greenStackView.spacing = 2
+        blueStackView.spacing = 2
+        headerTopConstraint.constant = 15
+        resetBottomConstraint.constant = 12
+        setColorBottomConstraint.constant = 12
+        infoBottomConstraint.constant = 12
+    }
+
+    func updateViewsForPortrait() {
+        headerStackView.axis = .vertical
+        headerLabel.textAlignment = .center
+        redStackView.spacing = 8
+        greenStackView.spacing = 8
+        blueStackView.spacing = 8
+        headerTopConstraint.constant = 20
+        resetBottomConstraint.constant = 20
+        setColorBottomConstraint.constant = 20
+        infoBottomConstraint.constant = 20
+    }
+
+    func styleViews() {
+        slidersContainer.layer.cornerRadius = 8
+        slidersContainer.layer.shadowOffset = CGSize(width: 0, height: 2)
+        slidersContainer.layer.shadowColor = UIColor.black.cgColor
+        slidersContainer.layer.shadowOpacity = 0.5
+        slidersContainer.layer.shadowRadius = 5
+        slidersContainer.layer.masksToBounds = false
+
+        resetButton.layer.cornerRadius = 4
+        resetButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        resetButton.layer.shadowColor = UIColor.black.cgColor
+        resetButton.layer.shadowOpacity = 0.5
+        resetButton.layer.shadowRadius = 5
+        resetButton.layer.masksToBounds = false
+
+        setColorButton.layer.cornerRadius = 4
+        setColorButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        setColorButton.layer.shadowColor = UIColor.black.cgColor
+        setColorButton.layer.shadowOpacity = 0.5
+        setColorButton.layer.shadowRadius = 5
+        setColorButton.layer.masksToBounds = false
+
+        infoButton.layer.cornerRadius = 4
+        infoButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        infoButton.layer.shadowColor = UIColor.black.cgColor
+        infoButton.layer.shadowOpacity = 0.5
+        infoButton.layer.shadowRadius = 5
+        infoButton.layer.masksToBounds = false
     }
 
     func setUpForRGB() {
