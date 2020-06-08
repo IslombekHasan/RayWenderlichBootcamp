634c3f21ac8938b961653d517d9f181e1aaa8bc5
Islombek Khasanov <islombekhasanov@gmail.com>
2020-06-08 21:43:45 +0500

    init: initializes RevBullsEye project

---
+++ Week02/RevBullsEye/RevBullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RevBullsEye
+//
+//  Created by Islombek Hasanov on 6/8/20.
+//  Copyright © 2020 Islombek Khasanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
