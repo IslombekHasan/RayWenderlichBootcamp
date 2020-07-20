0f6be4fdcecc7df1eba5803d84a10707a58a118c
Islombek Khasanov <islombekhasanov@gmail.com>
2020-07-20 19:04:04 +0500

    feat: adds loading data from JSON

--- Week08/SandwichSaturation_starter/SandwichSaturation/ViewControllers/SandwichViewController.swift
+++ Week08/SandwichSaturation_starter/SandwichSaturation/ViewControllers/SandwichViewController.swift
@@ -45,17 +45,18 @@ class SandwichViewController: UITableViewController, SandwichDataSource {
   }
 
   func loadSandwiches() {
-    let sandwichArray = [SandwichData(name: "Bagel Toast", sauceAmount: .none, imageName: "sandwich1"),
-                         SandwichData(name: "Bologna", sauceAmount: .none, imageName: "sandwich2"),
-                         SandwichData(name: "Breakfast Roll", sauceAmount: .none, imageName: "sandwich3"),
-                         SandwichData(name: "Club", sauceAmount: .none, imageName: "sandwich4"),
-                         SandwichData(name: "Sub", sauceAmount: .none, imageName: "sandwich5"),
-                         SandwichData(name: "Steak", sauceAmount: .tooMuch, imageName: "sandwich6"),
-                         SandwichData(name: "Dunno", sauceAmount: .tooMuch, imageName: "sandwich7"),
-                         SandwichData(name: "Torta", sauceAmount: .tooMuch, imageName: "sandwich8"),
-                         SandwichData(name: "Ham", sauceAmount: .tooMuch, imageName: "sandwich9"),
-                         SandwichData(name: "Lettuce", sauceAmount: .tooMuch, imageName: "sandwich10")]
-    sandwiches.append(contentsOf: sandwichArray)
+    guard let sandwichesURL = Bundle.main.url(forResource: "sandwiches", withExtension: "json") else { return }
+    do {
+      let decoder = JSONDecoder()
+      let data = try Data(contentsOf: sandwichesURL)
+      sandwiches = try decoder.decode([SandwichData].self, from: data)
+    } catch let error {
+      let alert = UIAlertController(title: "Could not load sandwiches :[", message: "\(error.localizedDescription)", preferredStyle: .alert)
+      let action = UIAlertAction(title: "OK", style: .default)
+      alert.addAction(action)
+      present(alert, animated: true)
+    }
+
   }
 
   func saveSandwich(_ sandwich: SandwichData) {
